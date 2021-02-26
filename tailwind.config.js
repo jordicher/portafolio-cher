3ab10bc93096904a2c42b2b28aed1a9b1d1728bb
jordicher <jordi_sodiac@hotmail.com>
2021-02-26 18:14:27 +0100

    change tailwind export

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,10 +5,7 @@ module.exports = {
     extend: {
       colors: {
         'charcoal': "#264653",
-        'persian': "#2A9D8F",      
-        'orangeyellow': "#E9C46A",
-        'sandybrown': "F4A261",
-        'burnt': "#E76F51"
+        'persian': "#2A9D8F"    
       }
     },
   },
